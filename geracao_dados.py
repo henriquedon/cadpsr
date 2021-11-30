45beb86859e59ba879a2a8093a728b295b76273e
Henrique Don <henriquedon@users.noreply.github.com>
2021-11-30 01:28:04 -0300

    Update geracao_dados.py

--- geracao_dados.py
+++ geracao_dados.py
@@ -10,10 +10,11 @@ data_nasc = '1990-01-01'
 status = 'ATIVO'
 
 #Lista de Entidades Fictícias da Região de SBC e Sto. André
-entidades = [['CRAS SBC - CENTRO', 'ATENDIMENTO', 'cras.sbc.centro@cadpsr.com','0000000000', 'Av. Brigadeiro Faria Lima, xx', '00000000', 'SAO BERNARDO DO CAMPO', 'SP'],
-             ['CREAS SBC', 'ATENDIMENTO', 'creas.sbc@cadpsr.com', '0000000000', 'Av. Rendenção, xx', '00000000', 'SAO BERNARDO DO CAMPO', 'SP'],
-             ['CRAS SANTO ANDRE - CENTRO', 'ATENDIMENTO', 'cras.stoandre.centro@cadpsr.com','00000000', 'Av. Ramiro Colleoni, xx', '00000000', 'SANTO ANDRE', 'SP'],
-             ['CREAS SANTO ANDRE', 'ATENDIMENTO', 'creas.sbc@cadpsr.com', '00000000', 'Av.Pereira Barreto, xx', '00000000', 'SANTO ANDRE', 'SP']]
+entidades = [['CRAS SANTO ANDRE - CENTRO', 'ATENDIMENTO', 'cras.stoandre.centro@cadpsr.com','00000000', 'Av. Ramiro Colleoni, xx', '00000000', 'SANTO ANDRE', 'SP'],
+             ['CRAS SBC - CENTRO', 'ATENDIMENTO', 'cras.sbc.centro@cadpsr.com','0000000000', 'Av. Brigadeiro Faria Lima, xx', '00000000', 'SAO BERNARDO DO CAMPO', 'SP'],
+             ['CREAS SANTO ANDRE', 'ATENDIMENTO', 'creas.sbc@cadpsr.com', '00000000', 'Av.Pereira Barreto, xx', '00000000', 'SANTO ANDRE', 'SP'],
+             ['CREAS SBC', 'ATENDIMENTO', 'creas.sbc@cadpsr.com', '0000000000', 'Av. Rendenção, xx', '00000000', 'SAO BERNARDO DO CAMPO', 'SP']
+             ]
 
 #Gravação das Entidades Fictícias no Banco de Dados
 for i in range(4):
@@ -31,28 +32,30 @@ for i in range(4):
     db.session.add(entidade)
     db.session.commit()
 
-senha = generate_password_hash('teste') # hash da senha expressa entre parênteses -- entre aspas simples!
+senha = input('Digite sua nova senha: ')
+senha = generate_password_hash(senha) # hash da senha expressa entre parênteses -- entre aspas simples!
+print('Hash de senha gerado com sucesso.')
 
 # Lista de Colaboradores
-colaboradores = [['João Fictício', '11111111111', data_nasc ,'gestor@cadpsr.com', senha,'GESTOR','GESTORA-SP'],
-                 ['Ada Lovelace','22222222222',data_nasc, 'ada.lovelace@cadpsr.com', senha, 'GERENTE', 'CRAS SBC - CENTRO'],
-                 ['Hipatia de Alexandria','33333333333',data_nasc, 'hipatia.alexandria@cadpsr.com', senha, 'GERENTE', 'CREAS SBC'],
-                 ['Tales de Mileto','44444444444',data_nasc, 'tales.mileto@cadpsr.com', senha, 'GERENTE', 'CRAS SANTO ANDRE - CENTRO'],
-                 ['John von Rambo','55555555555',data_nasc, 'john.rambo@cadpsr.com', senha, 'GERENTE','CREAS SANTO ANDRE']]
+colaboradores = [['Dev Grupo 6 Polo SBC', '11111111111', data_nasc ,'gestor@cadpsr.com', senha,'GESTOR','5']]
 
-for i in range(5):
-    colaborador = Colaborador(status='ATIVO',
-                              nome_civil=colaboradores[i][0],
-                              nome_social=colaboradores[i][0],
-                              cpf=colaboradores[i][1],
-                              data_nascimento=colaboradores[i][2],
-                              email=colaboradores[i][3],
-                              senha=colaboradores[i][4],
-                              tipo_clb=colaboradores[i][5],
-                              lotacao=colaboradores[i][6],
-                              data_criacao=agora,
-                              criado_por='dev-gr6')
-    db.session.add(colaborador)
-    db.session.commit()
 
-print('A criação de novos Colaboradores e Entidades foi efetudada com sucesso!')
+colaborador = Colaborador(status='ATIVO',
+                          nome_civil=colaboradores[0][0],
+                          nome_social=colaboradores[0][0],
+                          cpf=colaboradores[0][1],
+                          data_nascimento=colaboradores[0][2],
+                          email=colaboradores[0][3],
+                          senha=colaboradores[0][4],
+                          tipo_clb=colaboradores[0][5],
+                          lotacao=colaboradores[0][6],
+                          data_criacao=agora,
+                          criado_por='dev-gr6')
+db.session.add(colaborador)
+db.session.commit()
+colaborador = Colaborador.query.get(1)
+
+print('As Entidades e a crendencial do gestor foi criadas com sucesso.')
+print()
+print(f'E-mail/login de acesso: {colaborador.email}')
+print()
